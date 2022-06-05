3e6524e247460ad582e85b3d04359bc1d3095a07
Saetch <nils.wriedt@hotmail.com>
2022-06-06 01:26:15 +0200

    added simple async example (single threaded)

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::{thread::{self}, sync::{mpsc::{self, Sender, Receiver, TryRecvError, SendError}, Arc, Mutex, Weak}, time::Duration, task::Waker};
+use futures::{executor, Future};                
+fn main(){ 
+    let fut = main_async();
+    executor::block_on(fut);
+}
+
+fn mpsc_channel_async<T>() -> (MyMpscSender<T>, MpscReceiverFuture<T>){
+    let (s,r) = mpsc::channel();
+    let waker = Arc::new(Mutex::new(None));
+    let w = waker.clone();
+    let sender = MyMpscSender{sender: s, waker: w};
+    let w = Arc::downgrade(&waker);
+    let receiver = MpscReceiverFuture{receiver: r, waker_weak_p: w};
+    (sender, receiver)
+}
+
+
+async fn main_async(){
+    let (sender, receiver) = mpsc_channel_async();
+    let thread_handle = std::thread::spawn(move || {              //this thread is just here so the f1 function gets blocked by something and can later resume
+        wait_send_function(sender);
+    });
+    
+    let f1 = f1(receiver);
+    let f2 = f2();
+    futures::join!(f1, f2);
+    thread_handle.join().unwrap();
+}
+
+fn wait_send_function(sender: MyMpscSender<i32>){
+    thread::sleep(Duration::from_millis(5000));
+    sender.send(1234).unwrap();
+}
+
+
+async fn f1(receiver: MpscReceiverFuture<i32>){
+    println!("starting f1");
+    let new_nmbr = receiver.await.unwrap();               
+    println!("Received nmbr is: {}", new_nmbr);
+}
+
+async fn f2(){
+    println!("starting f2");
+}
+
+struct MyMpscSender<T>{                //this should be privated, so it can't actually be cloned or accessed?
+    sender: Sender<T>,           
+    waker: Arc<Mutex<Option<Waker>>>,
+}
+
+impl <T>MyMpscSender<T>{
+    pub fn send(&self, data: T )-> Result<(), SendError<T>>{
+        let ret = self.sender.send(data);
+        let mut lock = self.waker.lock().unwrap();
+        if let Some(waker) = lock.as_mut(){
+            waker.wake_by_ref();
+        }
+        ret
+    }
+}
+
+struct MpscReceiverFuture<T>{          
+    receiver: Receiver<T>,
+    waker_weak_p: Weak<Mutex<Option<Waker>>>,                  
+}
+
+
+impl <T>Future for MpscReceiverFuture<T> {
+    type Output = Result<T, TryRecvError>;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        let waker = cx.waker().clone();
+        if let Some(mutex) = self.waker_weak_p.upgrade(){
+            let mut lock = mutex.lock().unwrap();
+            *lock = Some(waker);
+        }
+        match self.receiver.try_recv(){
+            Ok(ret) => std::task::Poll::Ready(Ok(ret)),
+            Err(err) => match err {
+                TryRecvError::Empty => std::task::Poll::Pending,
+                TryRecvError::Disconnected => std::task::Poll::Ready(Err(err)),
+            },
+        }
+    }
+}
